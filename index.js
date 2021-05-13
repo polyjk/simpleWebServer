48c2319559454313a17dcfb633e813f33fa734bb
John <p5khamm@gmail.comm>
2021-05-13 17:12:26 -0500

    added getting a single resource

--- index.js
+++ index.js
@@ -30,6 +30,18 @@ app.get("/api/notes", (request, response) => {
   response.json(notes);
 });
 
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  console.log(id);
+  const note = notes.find((note) => note.id === id);
+  console.log(note);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
