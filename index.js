50e1b33615a26aa3895ba44051aa510676e8ae12
John <p5khamm@gmail.comm>
2021-07-15 13:08:46 -0500

    Routing frontend and backend mongo

--- index.js
+++ index.js
@@ -58,16 +58,37 @@ app.get("/api/notes", (request, response) => {
 //   response.json(notes);
 // });
 
+// app.get("/api/notes/:id", (request, response) => {
+//   const id = Number(request.params.id);
+//   console.log(id);
+//   const note = notes.find((note) => note.id === id);
+//   console.log(note);
+//   if (note) {
+//     response.json(note);
+//   } else {
+//     response.status(404).end();
+//   }
+// });
+
+// app.get("/api/notes/:id", (request, response) => {
+//   Note.findById(request.params.id).then((note) => {
+//     response.json(note);
+//   });
+// });
+
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  console.log(id);
-  const note = notes.find((note) => note.id === id);
-  console.log(note);
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      response.status(400).send({ error: "malformatted id" });
+    });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -99,25 +120,43 @@ app.put("/api/notes/:id", (request, response) => {
   response.json(note);
 });
 
+// app.post("/api/notes", (request, response) => {
+//   const body = request.body;
+
+//   if (!body.content) {
+//     return response.status(400).json({
+//       error: "content missing",
+//     });
+//   }
+
+//   const note = {
+//     content: body.content,
+//     important: body.important || false,
+//     date: new Date(),
+//     id: generateId(),
+//   };
+
+//   notes = notes.concat(note);
+
+//   response.json(note);
+// });
+
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 const unknownEndpoint = (request, response) => {
